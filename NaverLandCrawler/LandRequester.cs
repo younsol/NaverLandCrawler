db6eac1bd840ded00afee81f47760bc0a261a5e3
unknown <>
2017-02-07 20:46:45 +0900

    초기 커밋.

---
+++ NaverLandCrawler/LandRequester.cs
@@ -0,0 +1,37 @@
+﻿namespace NaverLandCrawler
+{
+    using NLog;
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    public class LandRequester
+    {
+        private Logger logger = LogManager.GetCurrentClassLogger();
+        private string landRequestUri = "http://land.naver.com/isale/isaleComplexList.nhn";
+
+        public async Task<string> Request()
+        {
+            logger.Info("Request Start");
+            try
+            {
+                var request = WebRequest.Create(landRequestUri) as HttpWebRequest;
+                request.Method = "POST";
+                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                var response = await request.GetResponseAsync();
+                var stream = response.GetResponseStream();
+                return await new StreamReader(stream).ReadToEndAsync();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e.StackTrace);
+            }
+            finally
+            {
+                logger.Info("Request Complete");
+            }
+            return null;
+        }
+    }
+}
---
+++ NaverLandCrawler/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace NaverLandCrawler
+{
+    using System.Threading;
+
+    class Program
+    {
+        private static LandRequester webRequester;
+
+        static void Main(string[] args)
+        {
+            webRequester = new LandRequester();
+            var response = webRequester.Request();
+            while(!response.IsCompleted)
+            {
+                Thread.Sleep(100);
+            }
+            
+            return;
+        }
+    }
+}
