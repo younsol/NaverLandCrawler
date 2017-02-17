2857d053e60bcf6e918b5c292182d9b4746a0cbc
정성훈 <younsol.r@gmail.com>
2017-02-18 01:09:31 +0900

    web deploy 안되는 부분 수정 web에서 좀 잘 보이게 가로 길이 조정

--- NaverLandCrawler/Program.cs
+++ NaverLandCrawler/Program.cs
@@ -20,13 +20,13 @@
             var targetDate = DateTime.Now.ToShortDateString().Replace("-", "");
             logger.Info($"[기준일시<{targetDate}> 정보수집 시작]");
             logger.Info("");
-            logger.Info($"==========================검색 옵션===================================");
+            logger.Info($"========================검색 옵션===========================");
             logger.Info($"[서울만?: {removeNotSeoul}]");
             logger.Info($"[경기만?: {removeNotGG}]");
             logger.Info($"[서울 또는 경기만?: {removeNotNearByRegion}]");
             logger.Info($"[1차 접수 기간이 지나지 않은것들만?: {removeFirstRegistPassed}]");
             logger.Info($"[1차 접수 기간이 공고 난 것들만?: {removeFirstRegistDateNotSpecified}]");
-            logger.Info($"====================================================================");
+            logger.Info($"============================================================");
             logger.Info("");
 
             LandRequester webRequester = new LandRequester();
@@ -61,14 +61,14 @@
                         continue;
                     }
 
-                    logger.Info($"====================================================================");
+                    logger.Info($"============================================================");
                     logger.Info($"[{complex.RegionName}] {complex.HscpNm}");
                     logger.Info($"{complex.IsaleYmdInfo} 공고 ");
                     logger.Info($"{complex.Ss3} ~ {complex.Sx3} 접수 ");
                     logger.Info($"{((float)complex.MinSpc / 3.3):f1} 평형 ~ {((float)complex.MaxSpc / 3.3):f1} 평형");
                     logger.Info($"{((float)complex.MinIsalePrc / 10000.0):f2} 억원 ~ {((float)complex.MaxIsalePrc / 10000.0):f2} 억원");
                     logger.Info($"{(int)(((float)(complex.MinIsalePrc + complex.MaxIsalePrc)) / ((float)(complex.MinSpc + complex.MaxSpc) / 3.3))} 만원/평");
-                    logger.Info($"====================================================================");
+                    logger.Info($"============================================================");
                     logger.Info("");
                     Thread.Sleep(500);
                 }
@@ -77,7 +77,7 @@
             logger.Info($"[정보 수집 완료]");
             Thread.Sleep(3000);
             File.Delete("\\\\NAS_for_younsol\\web\\index.html");
-            File.Move("index.html", "\\\\NAS_for_younsol\\web\\index.html");
+            File.Move("C:\\Users\\youns\\Desktop\\NaverLandCrawler\\index.html", "\\\\NAS_for_younsol\\web\\index.html");
         }
     }
 }
